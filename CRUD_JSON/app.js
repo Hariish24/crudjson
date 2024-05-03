fa4ac6a0b80dc421c14dffe7df70fc726f608adf
Hariish24 <lispostbox24@gmail.com>
2024-05-03 21:00:10 +0530

    adding

--- CRUD_JSON/app.js
+++ CRUD_JSON/app.js
@@ -1,6 +1,6 @@
 import express from "express"
 import dotenv from "dotenv"
-import apiRouter from "./router/apirouter.js"
+import apirouter from "./router/apirouter.js"
 
 
 dotenv.config({"path":"./setting/config.env"})
